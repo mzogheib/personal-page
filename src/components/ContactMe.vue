56c842fa9ea7279638bd8d4f79df595f417d7f1a
Marwan Zogheib <mzogheib@gmail.com>
2018-10-17 08:36:37 +1100

    [lint] Remove uneccesary escape

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -10,7 +10,7 @@ const setStyle = ({ inputBackground }) => {
 
 const isFormComplete = form => {
   // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-  const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const validEmail = form.email && EMAIL_REGEX.test(String(form.email).toLowerCase());
   return form.name && validEmail && form.subject && form.message;
 };
